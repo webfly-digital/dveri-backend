0243d09cb7639bf601237bd38ddc086aac9964f2
Alexey Sklyar <a.sklyar@semicvetic.com>
2025-03-19 13:58:07 +0700

    t#29917 - Форматирование шаблонов комплексного компонента новостей перед правкой.

--- bitrix/templates/1dvm/components/bitrix/news/service/bitrix/news.detail/.default/template.php
+++ bitrix/templates/1dvm/components/bitrix/news/service/bitrix/news.detail/.default/template.php
@@ -1,33 +1,38 @@
-<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
-<? if (empty($arResult["DETAIL_TEXT"])): ?>
+<?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+/** @var array $arResult */
+
+if (empty($arResult["DETAIL_TEXT"])) { ?>
     Раздел в разработке!
-<? else: ?>
-    <? if ($arResult["GALLERY"]): ?>
-    <div class="text-content">
-        <?= $arResult["DETAIL_TEXT"][0] ?>
+<?php } else { ?>
+    <?php if ($arResult["GALLERY"]) { ?>
+        <div class="text-content">
+            <?= $arResult["DETAIL_TEXT"][0] ?>
         </div>
-        <? $gallery = WFGeneral::GetGallery($arResult["PROPERTIES"]["GALLERY"]["VALUE"]); ?>
-        <? if ($gallery): ?>
+        <?php $gallery = WFGeneral::GetGallery($arResult["PROPERTIES"]["GALLERY"]["VALUE"]); ?>
+        <?php if ($gallery) { ?>
             <div class="gal gal-v1">
-                <? foreach ($gallery as $img):
-                      if (!empty($img["DESCRIPTION"]))
-                                $desc = $img["DESCRIPTION"];
-                            else
-                                $desc = $img["NAME"];?>
+                <?php foreach ($gallery as $img) {
+                    if (!empty($img["DESCRIPTION"])) {
+                        $desc = $img["DESCRIPTION"];
+                    } else {
+                        $desc = $img["NAME"];
+                    }
+                    ?>
                     <div class="gal-item">
                         <a href="<?= $img["PATH"] ?>" class="gal-item__preview">
-                            <img alt="<?=$desc?>" title="<?=$desc?>" class="lazyload" data-original="<?= $img["THUMB_PATH"] ?>">
+                            <img alt="<?= $desc ?>" title="<?= $desc ?>" class="lazyload" data-original="<?= $img["THUMB_PATH"] ?>">
                         </a>
                     </div>
-                <? endforeach ?>
+                <?php } ?>
             </div>
-        <? endif ?>
-    <div class="text-content">
-        <?= $arResult["DETAIL_TEXT"][1] ?> 
+        <?php } ?>
+        <div class="text-content">
+            <?= $arResult["DETAIL_TEXT"][1] ?>
         </div>
-    <? else: ?>
-    <div class="text-content">
-        <?= $arResult["DETAIL_TEXT"] ?> 
+    <?php } else { ?>
+        <div class="text-content">
+            <?= $arResult["DETAIL_TEXT"] ?>
         </div>
-    <? endif ?>
-            <? endif ?>
+    <?php } ?>
+<?php } ?>
