89ba045c9bb5d85fa9288005e60548bff03012ff
Alexey Sklyar <a.sklyar@semicvetic.com>
2025-04-10 15:21:16 +0700

    t#29915 - Замена на вызов метода getCompressedSrcUniversal и bugfix в коплексном компоненте news в шаблоне news.

--- bitrix/templates/1dvm/components/bitrix/news/news/bitrix/news.detail/.default/template.php
+++ bitrix/templates/1dvm/components/bitrix/news/news/bitrix/news.detail/.default/template.php
@@ -3,13 +3,13 @@
 /** @var array $arResult */
 ?>
 <div class="news-detail">
-    <?php if ($arResult["DETAIL_PICTURE"]): ?>
+    <?php if ($arResult["DETAIL_PICTURE"]) { ?>
         <div class="news-detail__cover">
-            <img src="<?= ImageCompressor::getCompressedSrc($arResult["DETAIL_PICTURE"]["ID"]) ?>"
+            <img src="<?= ImageCompressor::getCompressedSrcUniversal($arResult["DETAIL_PICTURE"]["SRC"]) ?>"
                  alt="<?= $arResult["DETAIL_PICTURE"]["ALT"] ?>"
                  title="<?= $arResult["DETAIL_PICTURE"]["TITLE"] ?>">
         </div>
-    <?php endif ?>
+    <?php } ?>
     <div class="text-content">
         <div class="news-detail__intro">
             <?php echo $arResult["PREVIEW_TEXT"]; ?>
--- bitrix/templates/1dvm/components/bitrix/news/news/bitrix/news.list/.default/template.php
+++ bitrix/templates/1dvm/components/bitrix/news/news/bitrix/news.list/.default/template.php
@@ -1,30 +1,31 @@
 <?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
 /** @var array $arResult */
+/** @var array $arParams */
 ?>
-<div class="news-list--rows">
-    <?php foreach ($arResult["ITEMS"] as $arItem): ?>
-        <?php
-        $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-        $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
-        ?>
-        <div class="news-card plugin-clickable" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
-            <div class="news-card__inner">
-                <?php if ($arItem["PREVIEW_PICTURE"]["SRC"]): ?>
-                    <div class="news-card__visual"
-                         style="background-image: url('<?= ImageCompressor::getCompressedSrc($arItem["PREVIEW_PICTURE"]["ID"]) ?>');">
-                    </div>
+    <div class="news-list--rows">
+        <?php foreach ($arResult["ITEMS"] as $arItem) { ?>
+            <?php
+            $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+            $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), ["CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')]);
+            ?>
+            <div class="news-card plugin-clickable" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
+                <div class="news-card__inner">
+                    <?php if ($arItem["PREVIEW_PICTURE"]["SRC"]) { ?>
+                        <div class="news-card__visual"
+                             style="background-image: url('<?= ImageCompressor::getCompressedSrcUniversal($arItem["PREVIEW_PICTURE"]["SRC"]) ?>');">
+                        </div>
 
-                <?php endif ?>
-                <div class="news-card__details">
-                    <h4 class="news-card__title"><a href="<?php echo $arItem["DETAIL_PAGE_URL"] ?>" class="link-detail"><?php echo $arItem["NAME"] ?></a></h4>
-                    <p class="news-card__intro"><?php echo $arItem["PREVIEW_TEXT"]; ?></p>
-                    <?php /*<p class="news-card__date"><? echo $arItem["DISPLAY_ACTIVE_FROM"] ?></p>*/?>
+                    <?php } ?>
+                    <div class="news-card__details">
+                        <h4 class="news-card__title"><a href="<?php echo $arItem["DETAIL_PAGE_URL"] ?>" class="link-detail"><?php echo $arItem["NAME"] ?></a></h4>
+                        <p class="news-card__intro"><?php echo $arItem["PREVIEW_TEXT"]; ?></p>
+                        <?php /*<p class="news-card__date"><? echo $arItem["DISPLAY_ACTIVE_FROM"] ?></p>*/ ?>
+                    </div>
                 </div>
             </div>
-        </div>
-    <?php endforeach; ?>
-</div>
-<?php if ($arParams["DISPLAY_BOTTOM_PAGER"]): ?>
+        <?php } ?>
+    </div>
+<?php if ($arParams["DISPLAY_BOTTOM_PAGER"]) { ?>
     <?= $arResult["NAV_STRING"] ?>
-<?php endif; ?>
+<?php } ?>
