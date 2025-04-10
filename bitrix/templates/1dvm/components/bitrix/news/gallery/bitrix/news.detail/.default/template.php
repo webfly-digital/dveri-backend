01a0d359d502866e0ac1574a2746abdcc0069fd3
Alexey Sklyar <a.sklyar@semicvetic.com>
2025-04-10 15:14:31 +0700

    t#29915 - Замена на вызов метода getCompressedSrcUniversal и bugfix в коплексном компоненте news в шаблоне gallery.

--- bitrix/templates/1dvm/components/bitrix/news/gallery/bitrix/news.detail/.default/template.php
+++ bitrix/templates/1dvm/components/bitrix/news/gallery/bitrix/news.detail/.default/template.php
@@ -5,8 +5,8 @@
 $gallery = WFGeneral::GetGallery($arResult["PROPERTIES"]["GALLERY"]["VALUE"]); ?>
 <div class="gal gal-v2">
     <?php
-    if ($gallery):
-        foreach ($gallery as $production):
+    if ($gallery) {
+        foreach ($gallery as $production) {
             $pos = '';
             $desc = '';
             $art = '';
@@ -17,22 +17,22 @@ $gallery = WFGeneral::GetGallery($arResult["PROPERTIES"]["GALLERY"]["VALUE"]); ?
             }
             ?>
             <div class="gal-item">
-                <?php if (!empty($art)): ?>
+                <?php if (!empty($art)) { ?>
                     <div class="gal-item-article"><?= $art ?></div>
-                <?php endif ?>
-                <?php if (!empty($production["KEYWORDS"])): ?>
+                <?php } ?>
+                <?php if (!empty($production["KEYWORDS"])) { ?>
                     <div class="gal-item-price"><?= $production["KEYWORDS"] ?></div>
-                <?php endif ?>
+                <?php } ?>
                 <a href="<?= $production["PATH"] ?>" class="gal-item__preview"
                    title="<?= $production["NAME"] ?>"
-                   style="background-image: url('<?= ImageCompressor::getCompressedSrc($production["ID"]) ?>');">
+                   style="background-image: url('<?= ImageCompressor::getCompressedSrcUniversal($production["PATH"]) ?>');">
                 </a>
                 <div class="gal-item-subtitle"><?= $production["NAME"] ?></div>
-                <?php if (!empty($desc)): ?>
+                <?php if (!empty($desc)) { ?>
                     <div class="gal-item-info"><?= $desc ?></div>
-                <?php endif ?>
+                <?php } ?>
             </div>
-        <?php
-        endforeach;
-    endif; ?>
+            <?php
+        }
+    } ?>
 </div>
--- bitrix/templates/1dvm/components/bitrix/news/gallery/bitrix/news.list/.default/template.php
+++ bitrix/templates/1dvm/components/bitrix/news/gallery/bitrix/news.list/.default/template.php
@@ -1,24 +1,25 @@
 <?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
+/** @var array $arParams */
 /** @var array $arResult */
 ?>
     <div class="albums">
-        <?php foreach ($arResult["ITEMS"] as $arItem): ?>
+        <?php foreach ($arResult["ITEMS"] as $arItem) { ?>
             <?php
             $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
             $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), ["CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')]);
             ?>
             <div class="album plugin-clickable" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
-                <?php if ($arItem["PREVIEW_PICTURE"]["SRC"]): ?>
+                <?php if ($arItem["PREVIEW_PICTURE"]["SRC"]) { ?>
                     <div class="album__cover"
-                         style="background-image: url('<?= ImageCompressor::getCompressedSrc($arItem["PREVIEW_PICTURE"]["ID"]) ?>');">
+                         style="background-image: url('<?= ImageCompressor::getCompressedSrcUniversal($arItem["PREVIEW_PICTURE"]["SRC"]) ?>');">
                         <div class="inner"></div>
                     </div>
-                <?php endif ?>
+                <?php } ?>
                 <h6 class="album__title"><a class="link-detail" href="<?php echo $arItem["DETAIL_PAGE_URL"] ?>"><?php echo $arItem["NAME"] ?></a></h6>
             </div>
-        <?php endforeach; ?>
+        <?php } ?>
     </div>
-<?php if ($arParams["DISPLAY_BOTTOM_PAGER"]): ?>
+<?php if ($arParams["DISPLAY_BOTTOM_PAGER"]) { ?>
     <?= $arResult["NAV_STRING"] ?>
-<?php endif; ?>
+<?php }
